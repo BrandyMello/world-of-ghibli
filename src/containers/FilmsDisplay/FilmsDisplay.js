4ffbbabc84cbf3a320f75524f8ed23eeded8c73d
Brandy Mello <brandyamello@gmail.com>
2019-09-12 12:58:39 -0600

    Start styling

--- src/containers/FilmsDisplay/FilmsDisplay.js
+++ src/containers/FilmsDisplay/FilmsDisplay.js
@@ -4,39 +4,19 @@ import Film from '../Film/Film';
 
 const FilmsDisplay = ({films}) => {
   console.log(images)
-// const mappedImage = images.map((image, index) => {
-//     console.log(image[`film${index}`][1])
-//     // return mappedImages
-//   })
-
   const mappedFilms = films.map((film, index) => {
       return <Film
       key={film.id}
       title={film.title}
       description={film.description}
-        photo={images[`film${index}`][1]}
+      photo={images[`film${index}`][1]}
       />
     })
-
-  // const mappedImages = images.map((image, index) => {
-  //   console.log(image[`film${index}`][1])
-  //   // return mappedImages
-  // })
-  // const mappedFilms = films.map(film => {
-  //   return <Film
-  //     key={film.id}
-  //     title={film.title}
-  //     description={film.description}
-  //     // photo={photo}
-  //   />
-  //   })
-  
 
   return (
     
     <div>
       {mappedFilms}
-      {/* {mappedImages} */}
     </div>
   )
 }
